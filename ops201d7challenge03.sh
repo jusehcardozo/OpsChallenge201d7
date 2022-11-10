e5f7c4b98b894b687918c70616ac45233ae7097f
jusehcardozo <joseccardozo93@gmail.com>
2022-11-09 18:36:47 -0800

    commit is done

---
+++ ops201d7challenge03.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Script: ops201d7challenge03.sh                      
+# Author: Jose Cardozo                     
+# Date of latest revision:   11/09/2022   
+# Purpose:   Finish the Challenge 03   
+
+# Basic function
+print_something () {
+  echo Hello I am a function
+}
+print_something
+print_something
+
+foo=last
+$foo
