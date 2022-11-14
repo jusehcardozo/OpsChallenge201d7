40bba2d4359037372fec0e512be16c2fd15da836
Jose Cardozo <116588590+jusehcardozo@users.noreply.github.com>
2022-11-13 23:57:10 -0800

    Create ops201d7challenge05.sh

---
+++ ops201d7challenge05.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script:                       create a loop              
+# Author:                       Jose Cardozo
+# Date of latest revision:      11/13/2022
+# Purpose:                      Loop
+
+# Basic for loop
+names="Stan Kyle Cartman"
+for name in $names
+do
+  echo $name
+done
+
+echo "Script complete"
+
+# End
+
+
+Could not do it, will get back to it and try again later.
