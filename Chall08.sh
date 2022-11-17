85fcf0550caeb95ca2e148d8b438844d54973b07
Jose Cardozo <116588590+jusehcardozo@users.noreply.github.com>
2022-11-16 20:47:11 -0800

    Create Chall08.sh

---
+++ Chall08.sh
@@ -0,0 +1,5 @@
+I've watched a youtube video explaining how to use ROBOCOPY but I've failed. I did not find on my computer the application called Note Pad++, there is only Note Pad.
+Also was afraid because I was trying to do this on my personal computer and I don't have a Virtual Box here. So I was afraid of messing up something on my personal PC.
+Here is the link of the video I watched and tried to follow the steps:
+
+https://www.youtube.com/watch?v=Xbo65eNq6fs
